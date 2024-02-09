c4a15722335e12edd6429a0adec68924182f0d07
Nicotiraboschi <nico.tiraboschi@gmail.com>
2024-02-09 14:39:44 +0100

    never localhost?

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const cors = require('cors');
 const { Server } = require('socket.io');
 const path = require('path');
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+// const port = process.env.PORT || 3000;
 
 app.use(cors());
 
